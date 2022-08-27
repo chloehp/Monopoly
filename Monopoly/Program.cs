5b7db0b6291e661718a1950b58fce5cf6dbf9c54
C HP <chloe.hughespenzer@gmail.com>
2022-08-27 17:06:01 +0100

    visuals

--- Monopoly/Program.cs
+++ Monopoly/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Policy;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Monopoly
@@ -51,7 +52,7 @@ namespace Monopoly
 
             switch (location)
             {
-                case 20:
+                case 21:
                     propertyName = "Jail";
                     rent = 0;
                     specialCase = 3;
@@ -67,7 +68,7 @@ namespace Monopoly
                     {
                         //stuck in jail
                         rent = 0;
-                        newLocation = 20;
+                        newLocation = 21;
                         specialCase = 2;
                     }
 
@@ -80,7 +81,7 @@ namespace Monopoly
                     break;
                 case 2:
                     propertyName = "Old Kent Road";
-                    rent = 20;
+                    rent = 21;
                     newLocation = location;
                     specialCase = 0;
                     break;
@@ -117,9 +118,9 @@ namespace Monopoly
                     }
                     else //d3 == 3
                     {
-                        //sends player to 20: Jail
+                        //sends player to 21: Jail
                         rent = 0;
-                        newLocation = 20;
+                        newLocation = 21;
                         specialCase = 7;
                     }
                     break;
@@ -138,8 +139,8 @@ namespace Monopoly
                 case 8:
                     propertyName = "Go to Jail!";
                     rent = 0;
-                    //sends player to 20: Jail
-                    newLocation = 20;
+                    //sends player to 21: Jail
+                    newLocation = 21;
                     specialCase = 8;
                     break;
                 case 9:
@@ -176,11 +177,12 @@ namespace Monopoly
 
             }
         }
-        public static void SpecialCase(int specialCase, string playerName, int money)
+        public static void SpecialCase(int specialCase, string playerName, out int newMoney)
         {
+            newMoney = 0;
             if (specialCase == 8)
             {
-                Console.WriteLine($"{playerName} is going to jail!");
+                Console.WriteLine($"{playerName} is going to jail!");                
             }
             if (specialCase == 4)
             {
@@ -201,17 +203,52 @@ namespace Monopoly
             if (specialCase == 5)
             {
                 Console.WriteLine($"{playerName} wins the lottery and is given £2000!");
-                money += 2000;
+                newMoney += 2000;
             }
             if (specialCase == 6)
             {
                 Console.WriteLine($"{playerName} gets a black eye and loses £2000 trying to double up in the casino.");
-                money -= 2000;
+                newMoney -= 2000;
             }
             if (specialCase == 7)
             {
                 Console.WriteLine($"{playerName} was sent to jail for a bank robbery gone wrong.");
             }
+
+
+        }
+    }
+    internal class Display
+    {
+        public static void Board(int player, int location, int diceRoll)
+        {
+            string[] array = new string[12];
+            int count = 0;
+            foreach (string a in array)
+            { array[count] = "  "; count++; }
+
+            if ((location >= 0) && (location <= array.Length))
+            {
+                switch (player)
+                {
+                    case 1:
+                        array[location - 1] = "▓▄";
+                        break;
+                    case 2:
+                        array[location - 1] = "█@";
+                        break;
+                    default:
+                        array[location - 1] = "??";
+                        break;
+                }
+            }
+
+            /* test display functions
+            foreach (string b in array)
+            { Console.WriteLine(b); }
+            */
+
+            Console.WriteLine($" _______________________________\r\n|Go     |KentRd |Jail   |Strand |\r\n|  {array[0]}   |  {array[1]}   |  {array[2]}   |  {array[3]}   |\r\n|_______|_______|_______|_______|\r\n|VineSt |               |Chance |\r\n|  {array[11]}   |               |  {array[4]}   |\r\n|_______|      [{diceRoll}]      |_______|\r\n|WhiteCh|               |FleetSt|\r\n|  {array[10]}   |               |  {array[5]}   |\r\n|_______|_______________|_______|\r\n|ParkLn |Mayfair|GoJail |Oxford |\r\n|  {array[9]}   |  {array[8]}   |  {array[7]}   |  {array[6]}   |\r\n|_______|_______|_______|_______|\r\n");
 
         }
     }
@@ -225,9 +262,12 @@ namespace Monopoly
     {    
         static void Main(string[] args)
         {
+            double version = 0.3;
+
             //Home screen
-            Console.WriteLine("MONOPOLY");
-            Console.WriteLine("\nHow to play:\nblah blah blah");
+            string logo = "██████████████████████████████████████████████████\r\n█▄─▀█▀─▄█─▄▄─█▄─▀█▄─▄█─▄▄─█▄─▄▄─█─▄▄─█▄─▄███▄─█─▄█\r\n██─█▄█─██─██─██─█▄▀─██─██─██─▄▄▄█─██─██─██▀██▄─▄██\r\n▀▄▄▄▀▄▄▄▀▄▄▄▄▀▄▄▄▀▀▄▄▀▄▄▄▄▀▄▄▄▀▀▀▄▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀";
+            Console.Write($"{logo}█████████version.{version}███");
+            Console.WriteLine("\n\nUsing the dice, move around a 12 tile Monopoly board.\r\nCollect properties, avoid jail-time, and win big!\r\n\r\nThe board consists of 1 go, 1 chance, 1 go to jail, 1 jail and 8 property tiles.\r\nThe game has two players: Boot and Dog.\r\nAll players start on GO and finish at the end of round 20.\r\nAll players start with £2000.");
             Console.WriteLine("\n[         PRESS SPACEBAR         ]");
 
             while (Console.ReadKey().Key != ConsoleKey.Spacebar) ;
@@ -249,12 +289,14 @@ namespace Monopoly
             int diceRoll;
             int rent;
             int money;
+            int newMoney = 0;
             int location;
             int newLocation;
+            bool goTrigger = false;
             string propertyName;
             int specialCase;
-            //array with what player owns which tile, -1 means non-property tile
-            int[] ownership = new int[20] { -1, 0, -1, 0, -1, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1 };
+            //array with what player owns which tile, -1 means non-ownable tile
+            int[] ownership = new int[21] { -1, 0, -1, 0, -1, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1 };
 
             Method object1 = new Method();
 
@@ -270,33 +312,47 @@ namespace Monopoly
                     location = newLocation;
 
                     //Console write . temp
-                    Console.WriteLine($"\nMONOPOLY      round: {round}");
+                    Console.WriteLine($"{logo}         round: {round}/20\n");
+                    Display.Board(player, location, diceRoll);
+
                     Console.WriteLine($"player name: {playerName}");
                     Console.WriteLine($"location ID: {location}");
                     Console.WriteLine($"money: {money}");
                     Console.WriteLine($"dice roll:{diceRoll}");
                     Console.WriteLine($"location name: {propertyName}");
                     Console.WriteLine($"rent: {rent}");
-                    Console.WriteLine("\n PRESS ENTER TO CONTINUE");
+
+
+                    Console.WriteLine("\n PRESS ENTER TO ROLL");
+
+                    while (Console.ReadKey().Key != ConsoleKey.Enter) ;
+                    Console.Clear();
 
                     // diceroll count to location. 20 exception
-                    while ((diceRoll > 0) && (location != 20))
+                    while ((diceRoll > 0) && (location != 21))
                     {
                         location++;
                         diceRoll--;
+
+                        //display animation:
+                        
+                        Console.SetCursorPosition(0, 5);
+                        Display.Board(player, location, diceRoll);
+                        Thread.Sleep(200);
+                        Console.Clear();
+                        
                         //for when player passes go
                         //if location ID is over 12, -12
                         if (location > 12)
                         {
-                            money += 200;
                             location -= 12;
-                            Console.WriteLine($"{playerName} is awarded £200 for passing/landing on GO!");
+                            goTrigger = true;
                         }
                     }
 
 
-                    while (Console.ReadKey().Key != ConsoleKey.Enter) ;
-                    Console.Clear();
+                    Console.WriteLine($"{logo}         round: {round}\n");
+                    Display.Board(player, location, diceRoll);
 
                     Method.TileIndex(diceRoll, location, player, out newLocation, out propertyName, out rent, out specialCase);
                     location = newLocation;
@@ -339,18 +395,26 @@ namespace Monopoly
                         }
                     }
 
+                    if (goTrigger == true)
+                    {
+                        Console.WriteLine($"{playerName} is awarded £200 for passing/landing on GO!");
+                        money += 200;
+                        goTrigger = false;
+                    }
                     //Special case messages / scenarios
-                    Method.SpecialCase(specialCase, playerName, money);
-
+                    Method.SpecialCase(specialCase, playerName, out newMoney);
+                    Console.WriteLine(newMoney);
+                    money += newMoney;
 
                     //Console write . temp
-                    Console.WriteLine($"\nMONOPOLY      round: {round}");
                     Console.WriteLine($"player name: {playerName}");
                     Console.WriteLine($"location ID: {location}");
                     Console.WriteLine($"money: {money}");
                     Console.WriteLine($"dice roll: {diceRoll}");
                     Console.WriteLine($"location name: {propertyName}");
                     Console.WriteLine($"rent: {rent}");
+
+
                     Console.WriteLine("\n[   NEXT PLAYER PRESS SPACEBAR   ]");
 
                     while (Console.ReadKey().Key != ConsoleKey.Spacebar) ;
@@ -372,7 +436,7 @@ namespace Monopoly
                 }
             }
 
-            Console.WriteLine($"\nGAME OVER\nBoot has ended with £{Boot.money}\nDog has ended with £{Dog.money}");
+            Console.WriteLine($"\nGAME OVER\nBoot has ended with £{Boot.money}\nDog has ended with £{Dog.money}\n");
             if (Boot.money > Dog.money)
             { Console.WriteLine("BOOT WINS"); }
             else if (Boot.money < Dog.money)
